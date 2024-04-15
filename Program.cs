538cb63726654b4115abab31e8370092ca2b081f
Lucas <lucasamarale@hotmail.com>
2024-04-15 17:45:40 -0300

    feat: enviando projeto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Lucas", sobrenome: "Cândido");
+
+Pessoa p2 = new Pessoa(nome: "João", sobrenome: "Silva");
+
+Curso ingles = new Curso()
+{
+    Nome = "Inglês",
+    Alunos = new List<Pessoa>()
+};
+
+ingles.AdicionarAluno(p1);
+ingles.AdicionarAluno(p2);
+ingles.ListarAlunos();
---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>ExemploExplorando</h1>
+<p>Projeto desenvolvido seguindo o módulo "Manipulando Dados e Objetos com .NET" do bootcamp ".NET Developer" da Digital Innovation One</p>
